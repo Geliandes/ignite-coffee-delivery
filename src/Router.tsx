573881d1a4d1fb7c23d6178a1e342f5926616526
Paulo Geliandes <geliandes@hotmail.com>
2023-07-16 13:00:51 -0300

    Update Router.tsx

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,17 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
 import { DefaultLayout } from './layouts/DefaultLayout'
 import { Home } from './pages/Home'
 import { Checkout } from './pages/Checkout'
 import { Success } from './pages/Success'
+import { useEffect } from 'react'
 
 export function Router() {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
   return(
     <Routes>
       <Route path='/' element={<DefaultLayout />}>
